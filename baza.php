f62162212bb0bf25e9bd00ae5ba1dd545e15008d
Podfacz <malpiawola@gmail.com>
2022-03-27 14:51:37 +0200

    name change

--- baza.php
+++ baza.php
@@ -4,6 +4,8 @@ if(isset($_POST['submit'])){
     $db= mysqli_connect($host, $log, $pass, $baza) or die("Połączenie nie udane");
     mysqli_close($db);
     header("Location: dodawanie.html?dodaj");
+}else{
+    
 }
 
 ?>
